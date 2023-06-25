5949d1436dae0d4ef1c1a713589be7a002871990
takahashi-pao <a.mapi.baseball@gmail.com>
2023-06-25 21:36:55 +0900

    ?

--- main.go
+++ main.go
@@ -19,21 +19,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ =
 			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+
+	t.templ.Execute(w, nil)
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					let's chat!
-				</body>
-			</html>
-		`))
-	})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// webサーバーを開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
